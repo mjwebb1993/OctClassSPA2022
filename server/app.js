67a6da2a6507de3f9fca52ba699cae38a68f4dd8
mjwebb1993 <mjwebb1993@yahoo.com>
2022-12-15 15:54:58 -0600

    Post request for pizza route added

--- server/app.js
+++ server/app.js
@@ -1,13 +1,21 @@
 // 'Import' the Express module instead of http
 const express = require("express");
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const pizzas = require("./routers/pizzas");
 
 dotenv.config();
 
-const PORT = process.env.PORT || 4040; // we use || to provide a default value
-
 // Initialize the Express application
 const app = express();
+
+mongoose.connect(process.env.MONGODB);
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "Connection Error:"));
+db.once(
+  "open",
+  console.log.bind(console, "Successfully opened connection to Mongo!")
+);
 
 const logging = (request, response, next) => {
   console.log(`${request.method} ${request.url} ${Date.now()}`);
@@ -20,6 +28,10 @@ app.use(logging);
 // Handle the request with HTTP GET method from http://localhost:4040/status
 app.get("/status", (request, response) => {
   response.status(200).json({ message: "Service healthy" });
+});
+app.get("/echo/:input", (request, response) => {
+  const message = request.params.input;
+  response.status(418).json({ echo: message });
 });
 
 app.get("/users/:id", (request, response) => {
@@ -46,6 +58,10 @@ app.post("/add", (request, response) => {
   };
   response.json(responseBody);
 });
+
+app.use("/pizzas", pizzas);
+
+const PORT = process.env.PORT || 4040; // we use || to provide a default value
 
 // Tell the Express app to start listening
 // Let the humans know I am running and listening on 4040
