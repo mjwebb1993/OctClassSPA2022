4b934efe64ed68f8433a9a931525e8f9a52ee78e
mjwebb1993 <mjwebb1993@yahoo.com>
2022-12-29 19:39:03 -0600

    added cors

--- server/app.js
+++ server/app.js
@@ -17,23 +17,23 @@ db.once(
   console.log.bind(console, "Successfully opened connection to Mongo!")
 );
 
-// CORS Middleware
-const cors = (request, response, next) => {
-  response.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type, Accept,Authorization,Origin"
-  );
-  response.setHeader("Access-Control-Allow-Origin", "*");
-  response.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-  response.setHeader("Access-Control-Allow-Credentials", true);
+const logging = (request, response, next) => {
+  console.log(`${request.method} ${request.url} ${Date.now()}`);
   next();
 };
 
-const logging = (request, response, next) => {
-  console.log(`${request.method} ${request.url} ${Date.now()}`);
+// CORS Middleware
+const cors = (req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type, Accept,Authorization,Origin"
+  );
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
   next();
 };
 
@@ -45,7 +45,6 @@ app.use(logging);
 app.get("/status", (request, response) => {
   response.status(200).json({ message: "Service healthy" });
 });
-
 app.get("/echo/:input", (request, response) => {
   const message = request.params.input;
   response.status(418).json({ echo: message });
